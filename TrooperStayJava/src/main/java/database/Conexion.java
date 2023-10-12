699669b04e960e452d2f6179a6f7ff644e2f3f7f
Lassa <nelsonlassa63@gmail.com>
2023-10-11 21:58:32 -0300

    update

--- TrooperStayJava/src/main/java/database/Conexion.java
+++ TrooperStayJava/src/main/java/database/Conexion.java
@@ -1,12 +1,16 @@
 package database;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.util.Properties;
 
 public class Conexion {
 
     public Connection conexion;
 
+    // TODO: ANTIGUO CONECTION
     public Conexion() {
         try {
             conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/trooper_stay", "root", "root");
@@ -20,3 +24,27 @@ public class Conexion {
     }
 
 }
+
+/* TODO: Este se va a reemplazar por el antiguo conection, EL VIERNES
+public Conexion() {
+        try {
+            Properties properties = new Properties();
+            try {
+                FileInputStream fileInputStream = new FileInputStream("src/main/resources/database.properties");
+                properties.load(fileInputStream);
+                fileInputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+                return;
+            }
+
+            conexion = DriverManager.getConnection(
+                    properties.getProperty("DB_URL"),
+                    properties.getProperty("DB_USERNAME"),
+                    properties.getProperty("DB_PASSWORD")
+            );
+        } catch (Exception e) {
+            System.err.print("Error" + e);
+        }
+    }
+ */
